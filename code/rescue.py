8bd69eeb4f4f292ce06302ff05c4fc678c030f5a
Rafael Estrella <123432865+RafaelEstrella05@users.noreply.github.com>
2024-10-12 11:46:29 -0500

    added proximity sensor pointing forward with graph to visualize the values

--- code/rescue.py
+++ code/rescue.py
@@ -1,66 +1,66 @@
-#vision sensor script for the quadcopter
+
+#vision sensor and proximity sensor script for the quadcopter
 
 # dependencies: pyzmq, cbor2
 #py -m pip install pyzmq cbor2
 
 def sysCall_init():
-    sim = require('sim');
+    sim = require('sim')
 
-    # do some initialization here
-    #
-    # Instead of using globals, you can do e.g.:
-    # self.myVariable = 21000000
-    global vision;
-    vision = sim.getObject('/Quadcopter/Vision_sensor');
+    # Initialize Vision Sensor
+    global vision
+    vision = sim.getObject('/Quadcopter/Vision_sensor')
 
-    global graph;
-    global graph_red;
-    global graph_green;
-    global graph_blue;
-    graph = sim.getObject('/VisionGraph');
+    # Initialize Vision Graph
+    global graph
+    global graph_red
+    graph = sim.getObject('/VisionGraph')
     graph_red = sim.addGraphStream(graph, 'red', '-', 0, [1,0,0])
-    #graph_green = sim.addGraphStream(graph, 'green', '-', 0, [0,1,0])
-    #graph_blue = sim.addGraphStream(graph, 'blue', '-', 0, [0,0,1])
 
-
-    pass
+    # Initialize Proximity Sensor and Graph
+    global proximity_sensor
+    global proximity_graph
+    global proximity_stream
+    proximity_sensor = sim.getObject('/Quadcopter/Proximity_sensor')
+    proximity_graph = sim.getObject('/ProximityGraph')
+    proximity_stream = sim.addGraphStream(proximity_graph, 'proximity', '-', 0, [0,1,0])
 
 def sysCall_actuation():
-    # put your actuation code here
+    # Actuation code here
     pass
 
 def sysCall_sensing():
-    # put your sensing code here
-    '''
-    packet1: default auxiliary packet of 15 auxiliary values: the minimum of [intensity red green blue depth], the maximum of [intensity red green blue depth], and the average of [intensity red green blue depth]
-    int result, list packet1, list packet2, etc. = sim.readVisionSensor(int visionSensorHandle)    
-    '''
-    result,packet1,packet2= sim.readVisionSensor(vision);
-    avg_red = packet1[11];
-    avg_green = packet1[12];
-    avg_blue = packet1[13];
+    # Vision sensor code
+    result,packet1,packet2 = sim.readVisionSensor(vision)
+    avg_red = packet1[11]
+    avg_green = packet1[12]
+    avg_blue = packet1[13]
 
-    #normalize the values
-    avg = (avg_red * avg_red + avg_green * avg_green + avg_blue * avg_blue);
-
+    # Normalize the values
+    avg = (avg_red * avg_red + avg_green * avg_green + avg_blue * avg_blue)
     
-    red = avg_red / avg;
-    green = avg_green / avg;
-    blue = avg_blue / avg;
+    red = avg_red / avg
+    green = avg_green / avg
+    blue = avg_blue / avg
     
-    #print(avg_red);
-    #print red green blue values
-    print("Red: ", red, "Green: ", green, "Blue: ", blue);
+    # Print red, green, blue values
+    print("Red: ", red, "Green: ", green, "Blue: ", blue)
+    
+    # Plot red value on VisionGraph
+    sim.setGraphStreamValue(graph, graph_red, red)
 
-    #plot the values
-    sim.setGraphStreamValue(graph, graph_red, red);
-    #sim.setGraphStreamValue(graph, graph_green, green);
-    #sim.setGraphStreamValue(graph, graph_blue, blue);
-
-    pass
+    # Proximity sensor code
+    proximity_state, proximity_distance = sim.readProximitySensor(proximity_sensor)
+    
+    if proximity_state > 0:  # If the proximity sensor detects something
+        # Plot the proximity sensor value
+        sim.setGraphStreamValue(proximity_graph, proximity_stream, proximity_distance)
+        print("Proximity Detected, Distance: ", proximity_distance)
+    else:
+        # No detection, plot a default value
+        sim.setGraphStreamValue(proximity_graph, proximity_stream, 0)
+        print("No Proximity Detection")
 
 def sysCall_cleanup():
-    # do some clean-up here
+    # Clean-up code here
     pass
-
-# See the user manual or the available code snippets for additional callback functions and details
